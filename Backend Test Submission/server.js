74948f83c168f14f4631e1ca7be14f34aa21cfcb
Sanjay S <sanjays13408@gmail.com>
2025-06-26 17:58:39 +0530

    validity added

--- Backend Test Submission/server.js
+++ Backend Test Submission/server.js
@@ -10,6 +10,12 @@ const urlDB = {};
 app.post('/shorturls', (req, res) => {
   const { url,validity,shortcode } = req.body;
   let shortenedId;
+
+  const now = Date.now();
+  const expirationTime = (validity && !isNaN(validity)) ?now + Number(validity) *60 *1000 : now + 30 *60 *1000;
+
+ 
+
   if(!shortcode){
     shortenedId = nanoid(6);
     urlDB[shortenedId] =  url;
@@ -18,14 +24,32 @@ app.post('/shorturls', (req, res) => {
     shortenedId = shortcode
     urlDB[shortenedId] = url;
   }
+
+  urlDB[shortenedId] = { url, expirationTime };
+
+
   const shortUrl = `http://localhost:${PORT}/${shortenedId}`;
-  res.json({shortUrl});
+  res.json({shortUrl, expirationTime});
   
 
 
 });
 
-app.use(auth);    
+app.get('/:shortId', (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = urlDB[shortId];
+    
+    if(!entry){
+      res.status(404).send('Short URL not found');
+    }
+
+    const now = Date.now();
+    if(now > entry.expirationTime){
+      delete urlDB[shortId];
+      res.status(404).send('The url expired');
+    }
+})
+  
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
