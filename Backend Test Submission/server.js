ec23730baf619e2d7d2fbff4796df2ec496419a6
Sanjay S <sanjays13408@gmail.com>
2025-06-26 17:48:05 +0530

    added shortener url

--- Backend Test Submission/server.js
+++ Backend Test Submission/server.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { nanoid } = require('nanoid');
 const app = express();
-const authRoute = require('./routes/auth')
+const PORT = 3000;
+app.use(bodyParser.json());
+
+const urlDB = {};
+
+app.post('/shorturls', (req, res) => {
+  const { url,validity,shortcode } = req.body;
+  let shortenedId;
+  if(!shortcode){
+    shortenedId = nanoid(6);
+    urlDB[shortenedId] =  url;
+  }
+  else{
+    shortenedId = shortcode
+    urlDB[shortenedId] = url;
+  }
+  const shortUrl = `http://localhost:${PORT}/${shortenedId}`;
+  res.json({shortUrl});
+  
+
+
+});
 
 app.use(auth);    
-  
+
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
